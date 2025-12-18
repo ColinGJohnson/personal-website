171d6873813b0811f0bed1fe24695f7f9557e635
Colin Johnson <colin@cgj.dev>
2025-12-18 01:10:04 -0800

    Add canvas resize observer

--- src/app.ts
+++ src/app.ts
@@ -1,147 +1,88 @@
 import './styles.css';
 import shaderCode from "./shaders/triangle.wgsl";
 
-(async () => {
-
-  // TODO: Finish implementing animated background
-  return
-
-  if (navigator.gpu === undefined) {
+async function main() {
+  const adapter = await navigator.gpu?.requestAdapter();
+  const device = await adapter?.requestDevice();
+  if (!device) {
     document.getElementById("webgpu-canvas").setAttribute("style", "display:none;");
     document.getElementById("no-webgpu").setAttribute("style", "display:block;");
     return;
   }
 
-  // Get a GPU device to render with
-  let adapter = await navigator.gpu.requestAdapter();
-  const device = await adapter.requestDevice();
-
-  // Get a context to display our rendered image on the canvas
   const canvas = document.getElementById("webgpu-canvas") as HTMLCanvasElement;
   const context = canvas.getContext("webgpu");
+  const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+    device,
+    format: presentationFormat,
+  });
 
-  // Setup shader modules
-  const shaderModule = device.createShaderModule({code: shaderCode});
-  const compilationInfo = await shaderModule.getCompilationInfo();
+  const module = device.createShaderModule({
+    label: 'Contour map shader module',
+    code: shaderCode,
+  });
 
-  if (compilationInfo.messages.length > 0) {
-    let hadError = false;
-    console.log("Shader compilation log:");
+  const pipeline = device.createRenderPipeline({
+    label: 'Contour map render pipeline',
+    layout: 'auto',
+    vertex: {
+      module,
+    },
+    fragment: {
+      module,
+      targets: [{ format: presentationFormat }],
+    },
+  });
 
-    for (let i = 0; i < compilationInfo.messages.length; ++i) {
-      const msg = compilationInfo.messages[i];
-      console.log(`${msg.lineNum}:${msg.linePos} - ${msg.message}`);
-      hadError = hadError || msg.type == "error";
-    }
+  function render() {
+    const renderPassDescriptor = {
+      label: 'Canvas renderPass',
+      colorAttachments: [
+        {
+          view: context.getCurrentTexture().createView(),
+          clearValue: [0.3, 0.3, 0.3, 1],
+          loadOp: 'clear',
+          storeOp: 'store',
+        },
+      ],
+    } as GPURenderPassDescriptor;
 
-    if (hadError) {
-      console.log("Shader failed to compile");
-      return;
-    }
+    const encoder = device.createCommandEncoder({
+      label: 'Command encoder'
+    });
+    const pass = encoder.beginRenderPass(renderPassDescriptor);
+    pass.setPipeline(pipeline);
+    pass.draw(3);
+    pass.end();
+
+    const commandBuffer = encoder.finish();
+    device.queue.submit([commandBuffer]);
   }
 
-  // Specify vertex data
-  // Allocate room for the vertex data: 3 vertices, each with 2 float4's
-  const dataBuf = device.createBuffer({size: 3 * 2 * 4 * 4, usage: GPUBufferUsage.VERTEX, mappedAtCreation: true});
-
-  // Interleaved positions and colors
-  new Float32Array(dataBuf.getMappedRange()).set([
-    1, -1, 0, 1,  // position
-    1, 1, 0, 1,   // color
-    -1, -1, 0, 1, // position
-    0, 1, 0, 1,   // color
-    0, 1, 0, 1,   // position
-    0, 0, 1, 1,   // color
-  ]);
-  dataBuf.unmap();
-
-  // Vertex attribute state and shader stage
-  const vertexState = {
-    // Shader stage info
-    module: shaderModule,
-    entryPoint: "vertex_main",
-    // Vertex buffer info
-    buffers: [{
-      arrayStride: 2 * 4 * 4,
-      attributes: [
-        {format: "float32x4", offset: 0, shaderLocation: 0},
-        {format: "float32x4", offset: 4 * 4, shaderLocation: 1}
-      ]
-    }]
-  };
-
-  // Setup render outputs
-  const swapChainFormat = "bgra8unorm";
-  context.configure({device: device, format: swapChainFormat, usage: GPUTextureUsage.RENDER_ATTACHMENT});
-
-  const depthFormat: GPUTextureFormat = "depth24plus-stencil8";
-  const depthTexture = device.createTexture({
-    size: { width: canvas.width, height: canvas.height, depth: 1 },
-    format: depthFormat,
-    usage: GPUTextureUsage.RENDER_ATTACHMENT
-  } as GPUTextureDescriptor); // TODO: Remove type assertion
-
-  const fragmentState = {
-    // Shader info
-    module: shaderModule,
-    entryPoint: "fragment_main",
-    // Output render target info
-    targets: [{format: swapChainFormat}]
-  };
-
-  // Create render pipeline
-  const layout = device.createPipelineLayout({bindGroupLayouts: []});
-
-  const renderPipeline = device.createRenderPipeline({
-    layout: layout,
-    vertex: vertexState,
-    fragment: fragmentState,
-    depthStencil: {format: depthFormat, depthWriteEnabled: true, depthCompare: "less"}
-  } as GPURenderPipelineDescriptor); // TODO: Remove type assertion
-
-  const renderPassDesc = {
-    colorAttachments: [{
-      // view: undefined,
-      loadOp: "clear",
-      clearValue: [0.1, 0.3, 0.3, 1],
-      storeOp: "store"
-    }],
-    depthStencilAttachment: {
-      view: depthTexture.createView(),
-      depthLoadOp: "clear",
-      depthClearValue: 1.0,
-      depthStoreOp: "store",
-      stencilLoadOp: "clear",
-      stencilClearValue: 0,
-      stencilStoreOp: "store"
+  // https://webgpufundamentals.org/webgpu/lessons/webgpu-resizing-the-canvas.html
+  const observer = new ResizeObserver(entries => {
+    for (const entry of entries) {
+      if (!(entry.target instanceof HTMLCanvasElement)) {
+        continue
+      }
+      const width = entry.devicePixelContentBoxSize?.[0].inlineSize ||
+          entry.contentBoxSize[0].inlineSize * devicePixelRatio;
+      const height = entry.devicePixelContentBoxSize?.[0].blockSize ||
+          entry.contentBoxSize[0].blockSize * devicePixelRatio;
+      const canvas = entry.target;
+      canvas.width = Math.max(1, Math.min(width, device.limits.maxTextureDimension2D));
+      canvas.height = Math.max(1, Math.min(height, device.limits.maxTextureDimension2D));
+      render();
     }
-  } as GPURenderPassDescriptor; // TODO: Remove type assertion
-
-  const animationFrame = function () {
-    let resolve = null;
-    const promise = new Promise(r => resolve = r);
-    window.requestAnimationFrame(resolve);
-    return promise
-  };
-  requestAnimationFrame(animationFrame);
-
-
-  while (true) {
-    await animationFrame();
-
-    for (const colorAttachment of renderPassDesc.colorAttachments) {
-      colorAttachment.view = context.getCurrentTexture().createView();
-    }
-
-    const commandEncoder = device.createCommandEncoder();
-
-    const renderPass = commandEncoder.beginRenderPass(renderPassDesc);
-
-    renderPass.setPipeline(renderPipeline);
-    renderPass.setVertexBuffer(0, dataBuf);
-    renderPass.draw(3, 1, 0, 0);
-
-    renderPass.end();
-    device.queue.submit([commandEncoder.finish()]);
+  });
+  try {
+    observer.observe(canvas, { box: 'device-pixel-content-box' });
+  } catch {
+    observer.observe(canvas, { box: 'content-box' });
   }
+}
+
+(async () => {
+  // void main();
 })();
--- src/shaders/triangle.wgsl
+++ src/shaders/triangle.wgsl
@@ -1,23 +1,27 @@
 
-struct VertexInput {
-    @location(0) position: vec4<f32>,
-    @location(1) color: vec4<f32>,
-};
-
-struct VertexOutput {
-    @builtin(position) position: vec4<f32>,
-    @location(0) color: vec4<f32>,
+struct OurVertexShaderOutput {
+  @builtin(position) position: vec4f,
 };
 
 @vertex
-fn vertex_main(vert: VertexInput) -> VertexOutput {
-    var out: VertexOutput;
-    out.color = vert.color;
-    out.position = vert.position;
-    return out;
-};
+fn vs(
+  @builtin(vertex_index) vertexIndex : u32
+) -> OurVertexShaderOutput {
+  let pos = array(
+    vec2f(-1.0,  3.0),
+    vec2f( 3.0, -1.0),
+    vec2f(-1.0, -1.0),
+  );
+
+  var vsOutput: OurVertexShaderOutput;
+  vsOutput.position = vec4f(pos[vertexIndex], 0.0, 1.0);
+  return vsOutput;
+}
 
 @fragment
-fn fragment_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(in.color);
+fn fs(
+  fsInput: OurVertexShaderOutput
+) -> @location(0) vec4f {
+  let hv = vec2f(floor(fsInput.position.xy % 2));
+  return vec4f(1, 0, 1, 1) * hv.x + vec4f(0, 1, 0, 1) * hv.y;
 }

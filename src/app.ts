e5028de0b90e13b307277f8fdfe346a26ce52886
Colin Johnson <colin@cgj.dev>
2025-12-28 19:56:43 -0800

    Extract resize observer class

--- src/app.ts
+++ src/app.ts
@@ -167,27 +167,7 @@ async function main() {
 
   requestAnimationFrame(render);
 
-  // https://webgpufundamentals.org/webgpu/lessons/webgpu-resizing-the-canvas.html
-  const observer = new ResizeObserver(entries => {
-    for (const entry of entries) {
-      if (!(entry.target instanceof HTMLCanvasElement)) {
-        continue
-      }
-      const width = entry.devicePixelContentBoxSize?.[0].inlineSize ||
-          entry.contentBoxSize[0].inlineSize * devicePixelRatio;
-      const height = entry.devicePixelContentBoxSize?.[0].blockSize ||
-          entry.contentBoxSize[0].blockSize * devicePixelRatio;
-      const canvas = entry.target;
-      canvas.width = Math.max(1, Math.min(width, device.limits.maxTextureDimension2D));
-      canvas.height = Math.max(1, Math.min(height, device.limits.maxTextureDimension2D));
-    }
-  });
-
-  try {
-    observer.observe(canvas, {box: 'device-pixel-content-box'});
-  } catch {
-    observer.observe(canvas, {box: 'content-box'});
-  }
+  new CanvasResizeObserver(canvas, device)
 }
 
 (async () => {
